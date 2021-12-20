3761204b6fb061ce5aa242a4fd26ef2fb74860ff
Crespin <crespin.christophe@netc.eu>
2021-12-20 11:21:26 +0100

    essais

--- src/be/technifutur/gestionInscriptions/ActivityType.java
+++ src/be/technifutur/gestionInscriptions/ActivityType.java
@@ -2,6 +2,24 @@ package be.technifutur.gestionInscriptions;
 
 public class ActivityType {
 
+    private String name;
+    private boolean registration;
 
+    public ActivityType (String name, boolean inscription) {
+        this.name = name;
+        this.registration = inscription;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean isRegistrationRequired() {
+        return b;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
 
 }
