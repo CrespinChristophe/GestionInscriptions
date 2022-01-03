77ca88cdd6b3000b6f0e570784e2503e8767cf8e
Crespin <crespin.christophe@netc.eu>
2022-01-03 15:43:24 +0100

    Dernier commit

--- src/be/technifutur/gestionInscriptions/Main.java
+++ src/be/technifutur/gestionInscriptions/Main.java
@@ -7,19 +7,6 @@ public class Main {
         ControlerCreerActivite controlerCreerActivite = new ControlerCreerActivite();
         controlerCreerActivite.setVue(activityVue);
         controlerCreerActivite.setModel(listActivityType);
-        /*int reponse = 1;
-        while (reponse != 0) {
-            switch (reponse) {
-                case 1 : controlerCreerActivite.call();
-                case 2 : contr
-
-            }
-
-
-
-
-
-        }*/
         controlerCreerActivite.call();
         ControlerSupprimerActivite controlerSupprimerActivite = new ControlerSupprimerActivite();
         controlerSupprimerActivite.setVue(activityVue);
