1fdede405d8a4a9911b8a19391c555de59ff35ae
Crespin <crespin.christophe@netc.eu>
2022-01-03 08:58:25 +0100

    Dernier commit

--- src/be/technifutur/gestionInscriptions/Main.java
+++ src/be/technifutur/gestionInscriptions/Main.java
@@ -3,8 +3,8 @@ package be.technifutur.gestionInscriptions;
 public class Main {
     public static void main(String[] args) throws Exception {
         ListActivityType listActivityType = new ListActivityType();
-        ControlerCreerActivite controleurCreerActivite = new ControlerCreerActivite();
         ActivityVue activityVue = new ActivityVue();
+        ControlerCreerActivite controleurCreerActivite = new ControlerCreerActivite();
         controleurCreerActivite.setVue(activityVue);
         controleurCreerActivite.setModel(listActivityType);
         controleurCreerActivite.call();
